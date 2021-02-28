3ab35bce241837ef7c5e212d5e908366e49db106
Nathan Bernardo <ncbernar@uci.edu>
2021-02-27 21:59:48 -0800

    actor-critic code in pytorch

---
+++ code/mlp.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Sequential
+from tensorflow.keras.models import Model
