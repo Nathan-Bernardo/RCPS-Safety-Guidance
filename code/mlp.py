1f3e232376eee96d40ef6ef2479f9506d819935a
Nathan Bernardo <50724423+Nathan-Bernardo@users.noreply.github.com>
2021-09-12 18:21:24 -0700

    Delete mlp.py

--- code/mlp.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow
-from tensorflow import keras
-from tensorflow.keras.layers import Dense, Sequential
-from tensorflow.keras.models import Model
-
-#Hello, just testing
